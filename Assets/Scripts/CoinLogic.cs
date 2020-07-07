2ec76f8d28c72329a43cd625fa4549bdbc2ba16a
Ashutosh Panchal <panchal.ashuto2704@gmail.com>
2020-07-07 21:44:26 +0530

    Final Commit
    Added Coins and Audio Clip.

---
+++ Assets/Scripts/CoinLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinLogic : MonoBehaviour
+{
+    AudioSource m_audioSource;
+
+    MeshRenderer m_meshRenderer;
+
+    Collider m_collider;
+
+    [SerializeField]
+    AudioClip CoinSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+        m_collider = GetComponent<Collider>();
+        m_meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, 1.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_audioSource.PlayOneShot(CoinSound);
+
+        m_meshRenderer.enabled = false;
+        m_collider.enabled = false;
+    }
+}
